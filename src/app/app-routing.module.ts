68f58a6ec747bd805542d5282d31789034b164ec
הנדסת תוכנה <huser@sem.local>
2024-05-01 21:05:00 +0300

    add components to call from server

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './Components/login/login.component';
-
+import { HomeComponent } from './Components/home/home.component';
 const routes: Routes = [
-  {path:'',component:LoginComponent}
+  {path:'',component:HomeComponent},
+  {path:'login',component:LoginComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './Components/login/login.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { HomeComponent } from './Components/home/home.component';
+import { JobComponent } from './Components/job/job.component';
+import { DetailsJobComponent } from './Components/details-job/details-job.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    
+    HomeComponent,
+    JobComponent,
+    DetailsJobComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,FormsModule,ReactiveFormsModule,HttpClientModule
-    
+    AppRoutingModule, FormsModule, ReactiveFormsModule, HttpClientModule, RouterModule
+
   ],
   providers: [
     provideClientHydration()
